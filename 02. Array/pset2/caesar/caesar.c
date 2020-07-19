c3f1518114276d52021767b5e65da2786ef7e154
bimonestle <bimonestle@yahoo.co.id>
2020-07-19 19:05:47 +0700

    add readKey(). Read argument argv[1]

--- 02. Array/pset2/caesar/caesar.c
+++ 02. Array/pset2/caesar/caesar.c
@@ -3,22 +3,30 @@
 #include <string.h>
 #include <ctype.h>
 
-int countArgv(int count, char *arg[]);
+int countArgv(int count);
+int readKey(char *arg[]);
 
 int main(int argc, char *argv[])
 {
-    countArgv(argc, &argv[0]);
+    countArgv(argc);
+    readKey(&argv[1]);
+
 }
 
-int countArgv(int count, char *arg[]) {
+int countArgv(int count) {
     if (count == 2)
     {
-        printf("Success");
+        printf("Success\n");
         return 1;
     }
     else
     {
-        printf("Usage: ./caesar {int key}");
+        printf("Usage: ./caesar {int key}\n");
         return 0;
     }
+}
+
+int readKey(char *arg[]) {
+    printf("The key is %s", *arg);
+    return 1;
 }
