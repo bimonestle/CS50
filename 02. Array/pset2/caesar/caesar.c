6a5813d463a23067ee8966e01b9a6072ed75cccb
bimonestle <bimonestle@yahoo.co.id>
2020-07-19 20:08:53 +0700

    add valKey(). Refactor code readKey(), countArgv()

--- 02. Array/pset2/caesar/caesar.c
+++ 02. Array/pset2/caesar/caesar.c
@@ -3,18 +3,21 @@
 #include <string.h>
 #include <ctype.h>
 
-int countArgv(int count);
+int countArgv(int count, char *arg[]);
 int readKey(char *arg[]);
+int valKey(char *arg[]);
 
 int main(int argc, char *argv[])
 {
-    countArgv(argc);
-    readKey(&argv[1]);
+    countArgv(argc, &argv[1]);
 
+    return 0;
 }
 
-int countArgv(int count) {
-    if (count == 2)
+int countArgv(int count, char *arg[]) {
+    int valid = valKey(arg);
+    
+    if (count == 2 && valid > 0)
     {
         printf("Success\n");
         return 1;
@@ -27,6 +30,25 @@ int countArgv(int count) {
 }
 
 int readKey(char *arg[]) {
-    printf("The key is %s", *arg);
+    printf("The key is %s\n", *arg);
+    valKey(arg);
+    return 1;
+}
+
+int valKey(char *arg[]) {
+    int length = strlen(*arg);
+    int result = 0;
+    // printf("ini: %s, %i\n", *arg,length);
+
+    for (int i = 0; i < length; i++)
+    {
+        int digit = isdigit(arg[0][i]);
+        if (digit < 1)
+        {
+            // printf("Usage: ./caesar {int key}\n");
+            return digit;
+            break;
+        }
+    }
     return 1;
 }
