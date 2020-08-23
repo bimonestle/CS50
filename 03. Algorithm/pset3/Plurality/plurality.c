fcee5a95b6b132ad3dbbb9500cb14c1174037e20
bimonestle <bimonestle@yahoo.co.id>
2020-08-23 16:02:34 +0700

    Initial commit & check argument count

--- 03. Algorithm/pset3/Plurality/plurality.c
+++ 03. Algorithm/pset3/Plurality/plurality.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
+    if (argc < 2)
+    {
+        printf("Please enter candidate's name");
+    }
+    else
+    {
+        printf("Success");
+    }
+    
+    
     return 0;
 }
