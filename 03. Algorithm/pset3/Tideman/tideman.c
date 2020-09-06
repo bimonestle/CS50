3ab762e65703b99e72be3f34b94571c8cef52fb3
bimonestle <bimonestle@yahoo.co.id>
2020-09-06 11:26:31 +0700

    add main() and pre-define variables & struct

--- 03. Algorithm/pset3/Tideman/tideman.c
+++ 03. Algorithm/pset3/Tideman/tideman.c
@@ -35,5 +35,44 @@ int main(int argc, char* argv[]) {
         printf("Usage: ./tideman [candidate ...]\n");
         return 1;
     }
+
+    // Populate array of candidates
+    candidateCount = argc - 1;
+    if (candidateCount > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidateCount; i++)
+    {
+        candidates[i] = *argv[i + 1];
+    }
+
+    // Clear graph locked in pairs
+    for (int i = 0; i < candidateCount; i++)
+    {
+        for (int j = 0; j < candidateCount; j++)
+        {
+            locked[i][j] = false;
+        }
+    }
+    
+    pairCount = 0;
+    printf("Number of voters: ");
+    int votersCount = scanf("%i", &votersCount);
+    
+    // Query for votes
+    for (int i = 0; i < votersCount; i++)
+    {
+        // ranks[i] is voter's ith preference
+        int ranks[candidateCount];
+
+        // Query for each ranks
+        for (int j = 0; j < candidateCount; j++)
+        {
+            /* code */
+        }
+        
+    }
     return 0;
 }
